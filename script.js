b712c5310a0f115b44ad6b02555a2959d7131da6
Alexandr <alexandrmail32@gmail.com>
2019-06-01 21:18:56 +0300

    Change age calculation method

--- script.js
+++ script.js
@@ -25,19 +25,9 @@ class Person {
   }
 
   __calculateAge(birthday) {
-    const nowDate = new Date();
-    const birthArray = birthday.split('.');
-    const year = nowDate.getFullYear();
-    const month = nowDate.getMonth() + 1;
-    const date = nowDate.getDate();
-    let age;
+    const [day, month, year] = this.birthday.split('.');
+    const age = new Date(Date.now() - +new Date(year, month, day)).getFullYear() -  new Date(0).getFullYear();
     
-    if(month < birthArray[1] || (month === birthArray[1] && date < birthArray[0])) {
-      age = year - birthArray[2] - 1;
-    }
-    if((month === birthArray[1] && date >= birthArray[0]) || month > birthArray[1]) {
-      age = year - birthArray[2];
-    }
     return age;
   }
 }
@@ -45,11 +35,11 @@ class Person {
 const dmytro = new Person('Dmytro', '26.11.1994', 1000);
 const pavel = new Person('Pavel', '06.06.1990', 400);
 
-dmytro.getInfo(); // print `Name: Dmytro, Age: 24, Amount: 1000$`
+dmytro.getInfo(); 
 dmytro.addMoney(2000, 'salary');
 dmytro.withdrawMoney(500, 'new phone');
-dmytro.getInfo(); // Name: Dmytro, Age: 24, Amount: 2500$
+dmytro.getInfo(); 
 dmytro.withdrawMoney(500, 'apartment rent');
-dmytro.getAccountHistory(); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
+dmytro.getAccountHistory(); 
 
-pavel.getInfo(); // // Name: Pavel, Age: 28, Amount: 400$
+pavel.getInfo(); 
