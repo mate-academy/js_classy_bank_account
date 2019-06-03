0957f9fe00af04d56e30c20b3cc1f6002a560a82
Stas Tselovanskyi <solmirx1@gmail.com>
2019-06-03 21:31:31 +0300

    fixedSomeErrors

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 class Person {
@@ -14,14 +13,14 @@ class Person {
 	}
 	 addMoney(fee, forWhat) {
 		this.amount += fee;
-		this.accountHistory.push(` '${forWhat}': ${this.amount} `);
+		this.accountHistory.push(`${forWhat}: ${this.amount}`);
 	}
 	getAccountHistory() {
 		console.log(`${this.accountHistory}`);
 	}
 	withdrawMoney(money, forWhat) {
 		this.amount -= money;
-		this.accountHistory.push(` '${forWhat}': -${money} `);
+		this.accountHistory.push(`${forWhat}: -${money}`);
 	}
 	 calculateAge(date) {
   		const splitDate = date.split('.').reverse();
@@ -34,12 +33,10 @@ class Person {
 const dmytro = new Person('Dmytro', '26.11.1994', 1000);
 const pavel = new Person('Pavel', '06.06.1990', 400);
 
-
 dmytro.getInfo(); // print `Name: Dmytro, Age: 24, Amount: 1000$`
 dmytro.addMoney(2000, 'salary');
 dmytro.withdrawMoney(500, 'new phone');
 dmytro.getInfo(); // Name: Dmytro, Age: 24, Amount: 2500$
 dmytro.withdrawMoney(500, 'apartment rent');
 dmytro.getAccountHistory(); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
-
 pavel.getInfo(); // // Name: Pavel, Age: 28, Amount: 400$
