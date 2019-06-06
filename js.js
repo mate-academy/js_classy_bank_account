d5c8f8d3c3c4e74e88d0d39357673bd92217593c
Antonenko Bohdan <bohdan1997@gmail.com>
2019-06-06 11:43:55 +0300

    'new_commit'

--- js.js
+++ js.js
@@ -2,43 +2,42 @@
 
 class Person {
   constructor(name, dateOfBirth, amount) {
-      this.name = name;
-      this.dateOfBirth = dateOfBirth.split('.').reverse();
-      this.amount = amount;
-      this.currentAmount = this.amount;
-      const [year, month, day] = this.dateOfBirth;
-      this.accountHistory = [{'Initial': amount}];
-      this.operations = function (value, key) {
-          this.accountHistory.push({[key]: value});
-      };
-      this.age = function () {
-          let age = currentYear - year;
-          const currentYear = new Date().getFullYear();
-          const currentMonth = new Date().getMonth() + 1;
-          const currentDay = new Date().getDate();
-          if (currentMonth < month || (currentMonth === month && currentDay < day)) {
-              return age - 1;
-          } else {
-              return age;
-          }
-      }
+    this.name = name;
+    this.amount = amount;
+    this.currentAmount = this.amount;
+    const [year, month, day] = dateOfBirth.split('.').reverse();;
+    this.accountHistory = [{'Initial': amount}];
+    this.operations = function (value, key) {
+        this.accountHistory.push({[key]: value});
+    };
+    this.age = function () {
+        const currentYear = new Date().getFullYear();
+        let age = currentYear - year;
+        const currentMonth = new Date().getMonth() + 1;
+        const currentDay = new Date().getDate();
+        if (currentMonth < month || (currentMonth === month && currentDay < day)) {
+            return age - 1;
+        } else {
+            return age;
+        }
+    }
   }
 
   getInfo () {
-      return (`${this.name} ${this.age()} ${this.currentAmount}`);
+    return (`${this.name} ${this.age()} ${this.currentAmount}`);
   };
 
   addMoney (value, str) {
-      this.currentAmount += value;
-      this.operations(value, str);
+    this.currentAmount += value;
+    this.operations(value, str);
   };
 
   getAccountHistory () {
-      return this.accountHistory;
+    return this.accountHistory;
   }
 
   withdrawMoney (value, str) {
-      this.currentAmount -= value;
-      this.operations(-value, str);
+    this.currentAmount -= value;
+    this.operations(-value, str);
   }
 }
