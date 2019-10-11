bccc11fbc3d2e05dfcb8d1c2081062148bece568
Viktoriia <jhunt@ukr.net>
2019-10-11 11:24:16 +0300

    fix

--- script.js
+++ script.js
@@ -5,41 +5,57 @@ class BankAccount {
     this.name = name;
     this.birth = birth;
     this.moneyAmount = moneyAmount;
-    this.accountHistory = [`Initial: ${moneyAmount}`];
+    this.accountHistory = [{name: 'Initial', value: this.moneyAmount}];
+    this.age = () => {
+      const date = this.birth.split('.');
+      const day = date[0];
+      const month = date[1];
+      const year = date[2];
+    
+      const yearNow = new Date().getFullYear();
+      const monthNow = new Date().getMonth() + 1;
+      const dayNow = new Date().getDate();
+    
+      const result = yearNow - year - 1;
+      const simpleResult = yearNow - year;
+    
+      if (monthNow === month && dayNow < day || monthNow < month) {
+        return result;
+      } else {
+        return simpleResult;
+      }
+    } 
+  }
+
+  createHistory(sum, service) {
+    this.accountHistory.push({name: service, value: sum});
   }
 
   getInfo() {
-    this.age = function() {
-      let date = this.birth.split('.');
-      let day = date[0];
-      let month = date[1];
-      let year = date[2];
-  
-      let yearNow = new Date().getFullYear();
-      let monthNow = new Date().getMonth() + 1;
-      let dayNow = new Date().getDate();
-  
-      if (monthNow === month && dayNow < day || monthNow < month) {
-        return yearNow - year - 1;
-      } else {
-        return yearNow - year;
-      }
-    }
-  
-    console.log(`Name: ${this.name}, Age: ${this.age(this.birth)}, Amount: ${this.moneyAmount}$`);
+    return `Name: ${this.name}, Age: ${this.age(this.birth)}, Amount: ${this.moneyAmount}$`;
   }
 
   addMoney(sum, service) {
     this.moneyAmount += sum;
-    this.accountHistory.push(`${service}: ${sum}`);
+    this.createHistory(sum, service);
+
+    return this.moneyAmount;
   }
 
-  withdrawMoney(withdrawSum, service) {
-    this.moneyAmount -= withdrawSum;
-    this.accountHistory.push(`${service}: -${withdrawSum}`);
+  withdrawMoney(sum, service) {
+    this.moneyAmount -= sum;
+    this.createHistory(sum, service);
+
+    return this.moneyAmount;
   }
 
   getAccountHistory() {
-    console.log(this.accountHistory);
+    const history = [];
+
+    for (let items of this.accountHistory) {
+      history.push(`${items.name}: ${items.value}`);
+    }
+
+    return history;
   }
 }
