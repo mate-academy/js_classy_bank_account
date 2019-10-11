849902c80db46ef66aa9b5345f5e56aedf70d3d2
Viktor Lukovets <nuerio63@gmail.com>
2019-10-11 14:58:05 +0300

    Solution

--- src/classy_bank_account.js
+++ src/classy_bank_account.js
@@ -4,35 +4,47 @@ class Person {
 
     constructor(name, dateOfBirth, initialAmount) {
         this.currentAmount = initialAmount;
-        this.accountHistory = [`Initial: ${initialAmount}`];
-        this.Name = name;
+        this.accountHistory = [
+            { name: 'Initial', value: initialAmount }
+        ];
+        this.name = name;
         this.Age = () => {
             const currentDate = new Date();
             let birthday = new Date(dateOfBirth.split('.').reverse().join('-'));
             var diff = currentDate.getTime() - birthday.getTime();
             return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
         };
-        this.Amount = `${this.currentAmount}$`;
+        this.amount = `${this.currentAmount}$`;
+    }
+
+    pushHist(accountHistory, value, name) {
+        accountHistory.push({ name, value });
     }
 
     getInfo() {
-        console.log(`Name: ${this.Name}, Age: ${this.Age()}, Amount: ${this.Amount}`);
+        return `Name: ${this.name}, Age: ${this.Age()}, Amount: ${this.amount}`;
     }
 
     addMoney(amount, info) {
         this.currentAmount += amount;
-        this.Amount = `${this.currentAmount}$`;
-        this.accountHistory.push(`${info}: ${this.currentAmount}`);
+        this.amount = `${this.currentAmount}$`;
+        this.pushHist(this.accountHistory, amount, info);
+        return `Current amount is ${this.currentAmount}$`;
     }
 
     withdrawMoney(amount, info) {
         this.currentAmount -= amount;
-        this.Amount = `${this.currentAmount}$`;
-        this.accountHistory.push(`${info}: -${amount}`);
+        this.amount = `${this.currentAmount}$`;
+        this.pushHist(this.accountHistory, -amount, info);
+        return `Current amount is ${this.currentAmount}`;
     }
 
     getAccountHistory() {
-        console.log(this.accountHistory);
+        let shortHist = [];
+        for (const obj of this.accountHistory) {
+            shortHist.push(`${obj.name}: ${obj.value}`);
+        }
+        return shortHist;
     }
 
 }
