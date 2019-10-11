bbefe4f8fa902815855de603a70a2878d2a768f0
Sijey <popov040391@gmail.com>
2019-10-11 14:44:35 +0300

    Solution

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
 'use strict';
 
-function calculateAge(dateOfBirth) {
-    const reverseDate = new Date(dateOfBirth.split('.').reverse());
-    const difference = Date.now() - reverseDate.getTime();
-    const msAge = new Date(difference);
-    return Math.abs(msAge.getFullYear() - 1970);
-}
-
 class Person {
     constructor(name, birthDay, amount) {
         this.name = name;
         this.birthDay = birthDay;
         this.amount = amount;
-        this.history = [`Initial: ${amount}`];
+        this.history = [{name: 'Initial', value: amount}];
+    }
+
+    _calculateAge(dateOfBirth) {
+        const reverseDate = new Date(dateOfBirth.split('.').reverse());
+        const difference = Date.now() - reverseDate.getTime();
+        const msAge = new Date(difference);
+        return Math.abs(msAge.getFullYear() - 1970);
+    }
+
+    addItems(amount, notes) {
+        this.amount += amount;
+        this.history.push({name: notes, value: amount});
     }
 
     getInfo() {
-        console.log(`Name: ${this.name}, 
-                 Age: ${calculateAge(this.birthDay)}, 
-                 Amount: ${this.amount}$`);
+        return `Name: ${this.name}, 
+                Age: ${this._calculateAge(this.birthDay)}, 
+                Amount: ${this.amount}$`;
     }
 
     addMoney(money, notes) {
-        this.amount += money;
-        this.history.push(`${notes}: ${money}`);
+        this.addItems(money, notes);
     }
 
     withdrawMoney(money, notes) {
-        this.amount -= money;
-        this.history.push(`${notes}: -${money}`);
+        this.addItems(-money, notes);
     }
 
     getAccountHistory() {
-        console.log(this.history);
+        const historyArr = [];
+        for (const key of this.history) {
+            historyArr.push(`${key.name}: ${key.value}`);
+        }
+
+        return historyArr;
     }
 }
 
 const dmytro = new Person('Dmytro', '26.11.1994', 1000);
 const pavel = new Person('Pavel', '06.06.1990', 400);
 
-dmytro.getInfo(); // print `Name: Dmytro, Age: <calculate yourself>, Amount: 1000$`
-dmytro.addMoney(2000, 'salary');
-dmytro.withdrawMoney(500, 'new phone');
-dmytro.getInfo(); // Name: Dmytro, Age: <calculate yourself>, Amount: 2500$
-dmytro.withdrawMoney(500, 'apartment rent');
-dmytro.getAccountHistory(); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
+console.log(dmytro.getInfo()); // print `Name: Dmytro, Age: <calculate yourself>, Amount: 1000$`
+console.log(dmytro.addMoney(2000, 'salary'));
+console.log(dmytro.withdrawMoney(1500, 'new phone'));
+console.log(dmytro.getInfo()); // Name: Dmytro, Age: <calculate yourself>, Amount: 2500$
+console.log(dmytro.withdrawMoney(500, 'apartment rent'));
+console.log(dmytro.getAccountHistory()); // [ 'Initial: 1000', 'salary: 2000', 'new phone: -500', 'apartment rent: -500']
 
-pavel.getInfo(); // // Name: Pavel, Age: <calculate yourself>, Amount: 400$
+console.log(pavel.getInfo()); // // Name: Pavel, Age: <calculate yourself>, Amount: 400$
