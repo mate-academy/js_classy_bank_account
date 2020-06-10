a25b5b77743b867eee7de2516a1a5073c4c0d024
LimonovInsp <bis.limonov@gmail.com>
2020-06-10 16:10:11 +0300

    solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,29 @@
 'use strict';
 
 class BankAccount {
-  // Write your code here
+  constructor(name, amount) {
+    this.name = name;
+    this.amount = amount;
+    this.accountHistory = [`Initial: ${this.amount}`];
+  }
+
+  getInfo() {
+    return `Name: ${this.name}, Amount: ${this.amount}$`;
+  }
+
+  addMoney(amount, operation) {
+    this.amount += amount;
+    this.accountHistory.push(`${operation}: ${amount}`);
+  }
+
+  withdrawMoney(amount, operation) {
+    this.amount -= amount;
+    this.accountHistory.push(`${operation}: -${amount}`);
+  }
+
+  getAccountHistory() {
+    return this.accountHistory;
+  }
 };
 
 module.exports = {
