51a0adfb30bd9f264ce343657c7f1734dfc9ed55
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-10 20:35:38 +0300

    first

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,31 @@
 'use strict';
 
 class BankAccount {
-  // Write your code here
+  constructor(name, amount, accountHistory) {
+    this.name = name;
+    this.amount = amount;
+    this.accountHistory = [`Initial: ${this.amount}`];
+  };
+
+  getInfo() {
+    return (`Name: ${this.name}, Amount: ${this.amount}$`);
+  }
+
+  addMoney(amount, info) {
+    this.amount += amount;
+
+    this.accountHistory.push(`${info}: ${amount}`);
+  }
+
+  withdrawMoney(amount, info) {
+    this.amount -= amount;
+
+    this.accountHistory.push(`${info}: -${amount}`);
+  }
+
+  getAccountHistory() {
+    return this.accountHistory;
+  }
 };
 
 module.exports = {
