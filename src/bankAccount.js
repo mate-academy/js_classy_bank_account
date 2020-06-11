a2f30ccdab20721764ab7eacf41e5dc8763d5138
YanG-6668 <goch.yannn@gmail.com>
2020-06-11 08:58:01 +0300

    class-1

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,29 @@
 'use strict';
 
 class BankAccount {
-  // Write your code here
+  constructor(name, amount) {
+    this.name = name;
+    this.amount = amount;
+    this.history = [`Initial: ${amount}`];
+  }
+
+  getInfo() {
+    return `Name: ${this.name}, Amount: ${this.amount}$`;
+  }
+
+  addMoney(amount, procedure) {
+    this.amount += amount;
+    this.history.push(`${procedure}: ${amount}`);
+  }
+
+  withdrawMoney(amount, procedure) {
+    this.amount -= amount;
+    this.history.push(`${procedure}: -${amount}`);
+  }
+
+  getAccountHistory() {
+    return this.history;
+  }
 };
 
 module.exports = {
