650b58432085be91caa50dc60fbbe5f557ba67f1
Glib Rulev <velzepooz@gmail.com>
2020-06-11 15:36:21 +0300

    Solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,38 @@
 'use strict';
 
 class BankAccount {
-  // Write your code here
+  constructor(name, birthdate, money) {
+    this.name = name;
+    this.birthdate = birthdate;
+    this.money = money;
+    this.accountHistory = [`Initial: ${this.money}`];
+  }
+
+  getAge() {
+    const birthdayArray = this.birthdate.split('.');
+    const birthday = new Date(
+      birthdayArray[2], birthdayArray[1] - 1, birthdayArray[0]);
+
+    return Math.trunc((Date.now() - birthday) / 31557600000);
+  }
+
+  getInfo() {
+    return `Name: ${this.name}, Age: ${this.getAge()}, Amount: ${this.money}$`;
+  }
+
+  addMoney(money, transactionName) {
+    this.money += money;
+    this.accountHistory.push(`${transactionName}: ${money}`);
+  }
+
+  withdrawMoney(money, transactionName) {
+    this.money -= money;
+    this.accountHistory.push(`${transactionName}: ${-money}`);
+  }
+
+  getAccountHistory() {
+    return this.accountHistory;
+  }
 }
 
 module.exports = {
