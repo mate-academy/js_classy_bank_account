25e9e4bba87e813a474dde38fcf1cfb9f64026e9
denkondratiev <denys.kondratiev.job@gmail.com>
2020-06-11 14:24:57 +0300

    fix

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,14 +1,14 @@
 'use strict';
 
 class BankAccount {
-  constructor(name, amount, accountHistory) {
+  constructor(name, amount) {
     this.name = name;
     this.amount = amount;
     this.accountHistory = [`Initial: ${this.amount}`];
   };
 
   getInfo() {
-    return (`Name: ${this.name}, Amount: ${this.amount}$`);
+    return `Name: ${this.name}, Amount: ${this.amount}$`;
   }
 
   addMoney(amount, info) {
