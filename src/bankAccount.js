b935508f62df978d7c165e7b93abcbfa0d53aaa5
German <germandolia@gmail.com>
2020-06-12 10:18:23 +0300

    solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,29 @@
 'use strict';
 
 class BankAccount {
-  // Write your code here
+  constructor(name, dayOfBirth, initialAmount) {
+    this.name = name;
+    this.amount = initialAmount;
+    this.history = [`Initial: ${initialAmount}`];
+  }
+
+  getInfo() {
+    return `Name: ${this.name}, Amount: ${this.amount}$`;
+  }
+
+  addMoney(value, reference) {
+    this.amount += value;
+    this.history.push(`${reference}: ${value}`);
+  }
+
+  withdrawMoney(value, reference) {
+    this.amount -= value;
+    this.history.push(`${reference}: -${value}`);
+  }
+
+  getAccountHistory() {
+    return this.history;
+  }
 }
 
 module.exports = {
