a4dcc26a8e096f5f207b697377e0266d4b404405
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-06-12 19:14:01 +0300

    solution

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,8 +1,36 @@
 'use strict';
 
 class BankAccount {
-  // Write your code here
-}
+  constructor(name, birthDate, amount) {
+    const date1 = new Date('2020.12.06');
+    const date2 = new Date(birthDate.split('.').reverse().join('.'));
+    const diff = date1 - date2;
+    const years = Math.floor(diff / 31536000000);
+
+    this.name = name;
+    this.amount = amount;
+    this.age = years;
+    this.history = ['Initial: ' + amount];
+  }
+
+  getInfo() {
+    return (`Name: ${this.name}, Age: ${this.age}, Amount: ${this.amount}$`);
+  }
+
+  addMoney(money, key) {
+    this.amount += money;
+    this.history.push(key + ': ' + money);
+  }
+
+  withdrawMoney(money, key) {
+    this.amount -= money;
+    this.history.push(key + ': -' + money);
+  }
+
+  getAccountHistory() {
+    return this.history;
+  }
+};
 
 module.exports = {
   BankAccount,
