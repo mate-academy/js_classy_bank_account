e7840ca530233767f44319f7cc7127811d2a9200
DTR <t.terry.rice@gmail.com>
2020-06-12 16:57:07 +0300

    0.9

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,7 +1,49 @@
 'use strict';
 
 class BankAccount {
-  // Write your code here
+  constructor(clientName, clientDate, clientMoney) {
+    this.name = clientName;
+    this.birthday = clientDate;
+    this.amount = clientMoney;
+    this.moneyHistory = [`Initial: ${clientMoney}`];
+  }
+
+  findAge(birthday) {
+    let birthdayFormat = birthday.split('.');
+
+    [birthdayFormat[0], birthdayFormat[1]]
+    = [birthdayFormat[1], birthdayFormat[0]];
+    birthdayFormat = birthdayFormat.join('.');
+
+    const actualDate = new Date().getTime();
+    const userBirthday = new Date(birthdayFormat).getTime();
+
+    return (Math.floor((actualDate - userBirthday)
+      / (365.25 * 24 * 3600 * 1000)));
+    //  years * days * seconds * milliseconds
+  }
+
+  getInfo() {
+    const age = this.findAge(this.birthday);
+
+    return `Name: ${this.name}, Age: ${age}, Amount: ${this.amount}$`;
+  }
+
+  addMoney(sum, operation) {
+    this.amount += sum;
+
+    return this.moneyHistory.push(`${operation}: ${sum}`);
+  }
+
+  withdrawMoney(sum, operation) {
+    this.amount -= sum;
+
+    return this.moneyHistory.push(`${operation}: -${sum}`);
+  }
+
+  getAccountHistory() {
+    return this.moneyHistory;
+  }
 }
 
 module.exports = {
