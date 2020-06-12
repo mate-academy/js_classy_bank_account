1a46e58057b42af995acea9972873194cbdd60a6
Vitalii Kuzmichov <43987305+Kussmich@users.noreply.github.com>
2020-06-12 23:35:57 +0300

    Solution

--- src/bankAccount.test.js
+++ src/bankAccount.test.js
@@ -14,7 +14,7 @@ describe('BankAccount', () => {
     const account = new BankAccount('Dmytro', '15.03.1997', 1000);
 
     expect(account.getInfo())
-      .toBe(`Name: Dmytro, Amount: 1000$`);
+      .toBe(`Name: Dmytro, Age: 23, Amount: 1000$`);
   });
 
   it('Should have an initial history', () => {
@@ -38,7 +38,7 @@ describe('BankAccount', () => {
     account.addMoney(400, 'lottery');
 
     expect(account.getInfo())
-      .toBe(`Name: Oleg, Amount: 2700$`);
+      .toBe(`Name: Oleg, Age: 22, Amount: 2700$`);
   });
 
   it('Should withdraw money', () => {
@@ -48,7 +48,7 @@ describe('BankAccount', () => {
     account.withdrawMoney(700, 'products');
 
     expect(account.getInfo())
-      .toBe(`Name: Oleg, Amount: 700$`);
+      .toBe(`Name: Oleg, Age: 22, Amount: 700$`);
   });
 
   it('Should update accountHistory', () => {
